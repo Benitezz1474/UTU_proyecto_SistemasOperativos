943305d9059ec444279d19d7831c1760b1fefda1
Fernando Benitez <benitezfernando1474.com>
2022-10-22 10:20:32 -0300

    Version 2 (Fallas)

--- Login.sh
+++ Login.sh
@@ -5,4 +5,18 @@ echo "Bienvenido al sistema de login"
 read -p "Ingrese su CI: " ci
 read -p "Ingrese su clave: " pass
 
-#consulta a la bd:
+#consulta a la bd, en mi PC da error:
+
+consulta = "SELECT tipo_user FROM usuarios WHERE ci = 'ci' AND clave = 'pass'";
+
+$cons = $(mysql -u root fbenitez -h localhost -e "$consulta");
+
+echo "$cons"
+
+#suponiendo que lo demas funcione; al devolver el tipo user, se ejecutará una accion,
+#que dependerá de este (tipo_user)
+
+#antes de entrar en el case, hay que cortar el comando para que funcione
+
+
+
